572d9a37e40caeaa46bf4e755f7ecbe6d8345bfc
Jessica Standage <Jessica@Jessicas-MacBook-Pro.local>
2019-06-23 17:44:04 -0700

    Update Assignment 4_Part 1_Tk2.js

--- Assignment 4_Part 1_Tk2.js
+++ Assignment 4_Part 1_Tk2.js
@@ -1,35 +1,36 @@
 // Assignment 4, Part 1 // My version of the ES2015 "Set" type
 
-let test_set = [1,2,3], new_set;
 
-function myset_add(array, 1,2,3) {
+function myset_add(anArray, aValue){
+  let test_set = [1,2,3], new_set;
 }
 
-new_set = myset_add.push(test_set, 1);
-if ( ! arrays_equal(new_set.sort(), [1,2,3]) ) {
+new_set = myset_add([1,2,3],4);
+if ( ! arrays_equal(new_set.sort(), [1,2,3,4]) ) {
   console.log("Failed adding existing data");
 }
 
-new_set = myset_add.push(test_set, 2);
+new_set = myset_add([1,2,3],4);
 if ( ! arrays_equal(new_set.sort(), [1,2,3,4]) ) {
   console.log("Failed adding new data");
 }
 
-new_set = myset_add.push(test_set, 3);
+new_set = myset_add.([1,2,3],4);
 if ( ! arrays_equal(new_set.sort(), [1,2,3,4]) ) {
   console.log("Failed adding new data");
 }
 
-function myset_remove(array, 1,2,3) {
+function myset_remove(anotherArray, anotherValue) {
+    let test_set = [1,2,3], new_set;
 }
 
 test_set = [1,2,3];
-new_set = myset_remove(test_set, 3);
+new_set = myset_remove(anotherArray, 3);
 if ( ! arrays_equal(new_set.sort(), [1,2]) ) {
   console.log("Remove failed");
 }
 
 // Return true if test_value is in the set, false otherwise function myset_has(data, test_value) { ... }
-function myset_has(data, test_value) { ... }
+function myset_has(anotherArray, anotherValue) { ... }
 
-function myset_size(data) { ... }
+function myset_size(anotherArray, anotherValue) { ... }
